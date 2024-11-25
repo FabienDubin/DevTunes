65c62c019279899865c7bfdae9c1adaf7c8dc14b
Emilia Junghanns <emijunghanns@hotmail.com>
2024-11-25 13:05:26 +0100

    homepage and 404 linked

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <p>Welcome to our Amazin APP</p>
+      <Link to="/search-album">
+        <button>ADD RECORDS TO MY COLLECTION</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h2>404 Not Found Page</h2>
+      <p>Try again</p>
+      <Link to="/search-album">
+        <button>Search for another album!</button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
