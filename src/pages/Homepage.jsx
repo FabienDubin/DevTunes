e33cbd934fb986f8fcc5071df6d324eb38a27db3
Emilia Junghanns <emijunghanns@hotmail.com>
2024-11-25 17:16:11 +0100

    homepage and notfound done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../pages/HomePage.css";
+import { Button } from "@/components/ui/button";
 
 const Homepage = () => {
   return (
-    <div>
-      <p>Welcome to our Amazin APP</p>
-      <Link to="/search-album">
-        <button>ADD RECORDS TO MY COLLECTION</button>
-      </Link>
+    <div className="homepage">
+      <div className="hometext">
+        <h1 className="homeh1">Welcome to DevTunes</h1>
+        <h2 className="homeh2">Your personal music adventure starts here. </h2>
+        <h3>Add your first album to begin!</h3>
+
+        <Link to="/search-album">
+          <Button className="startbutton" size="lg">
+            ADD ALBUM TO MY COLLECTION
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
