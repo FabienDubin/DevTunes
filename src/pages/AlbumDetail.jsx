89479dcee528b7161313b47bb85033b523d9fb9c
Fabien Dubin <fabien.dubin@gmail.com>
2024-11-25 17:14:04 +0100

    working on detailsD

--- src/pages/AlbumDetail.jsx
+++ src/pages/AlbumDetail.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const AlbumDetail = () => {
-  return <div>AlbumDetail</div>;
+const AlbumDetail = ({ access }) => {
+  //Getting album ID from the previous page
+  const { albumId } = useParams();
+
+  // console.log(access.access_token);
+
+  //ALBUM INFO VIA SPOTIFY API
+  const [album, setAlbum] = useState();
+  const [comments, setComments] = useState();
+  useEffect(() => {
+    async function getAlbumInfos() {
+      try {
+        console.log(access.access_token);
+        const { data } = await axios.get(
+          `https://api.spotify.com/v1/albums/${albumId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${access.access_token}`,
+            },
+          }
+        );
+        setAlbum(data);
+        console.log("got album datas", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    // wait to getting the token
+    if (access.access_token) {
+      getAlbumInfos();
+    }
+
+    // COMMENTS VIA JSON SERVER
+    async function getAlbumComments() {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:5005/comments?albumId=${albumId}`
+        );
+        setComments(data);
+        console.log("got comments ;)", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAlbumComments();
+  }, [albumId, access]);
+
+  return (
+    <div>
+      <img src={album.images[1].url} alt={album.name} />
+      <h1>{album.name}</h1>
+      <h2>{album.artists[0].name} </h2>
+    </div>
+  );
 };
 
 export default AlbumDetail;
