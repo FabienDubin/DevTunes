bf842a6761f26421bbedcf974a3ce99a234291b9
Fabien Dubin <fabien.dubin@gmail.com>
2024-11-26 22:49:35 +0100

    Jod done on deatil page. One patch remaining

--- src/pages/AlbumDetail.jsx
+++ src/pages/AlbumDetail.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../pages/AlbumDetail.css";
 import {
   Table,
@@ -27,9 +28,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { v4 as uuidv4 } from "uuid";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Headphones, Send, CirclePlus } from "lucide-react";
+import { Headphones, Send, CirclePlus, CircleMinus } from "lucide-react";
 import dateFormat, { masks } from "dateformat";
 const now = new Date();
 
@@ -90,7 +92,6 @@ const AlbumDetail = ({ access }) => {
     }
     getAlbumComments();
     // JSON Server to get users infos
-
     async function getUsers() {
       try {
         const { data } = await axios.get(`http://localhost:5005/users`);
@@ -101,6 +102,25 @@ const AlbumDetail = ({ access }) => {
       }
     }
     getUsers();
+
+    // JSON Server to check if the album is in the collection
+    async function checkIfInCOllection() {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:5005/users/${currentUser}`
+        );
+        const userCollection = data.collection || [];
+        const albumExists = userCollection.some(
+          (album) => album.id === albumId
+        );
+        setIsInCollection(albumExists);
+      } catch (error) {
+        console.log("error in chekIfInCollection", error);
+      }
+    }
+    if (album) {
+      checkIfInCOllection();
+    }
   }, [albumId, access]);
 
   //FUNCTIONS
@@ -114,29 +134,71 @@ const AlbumDetail = ({ access }) => {
   function getUsersbyId(userId) {
     return users.find((user) => user.id === userId);
   }
-  if (users) {
-    console.log("user found", getUsersbyId(3));
+  //   if (users) {
+  //     console.log("user found", getUsersbyId(3));
+  //   }
+
+  // JSON Server to add album to collection
+  async function addAlbumToCollection() {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:5005/users/${currentUser}`
+      );
+      const userCollection = data.collection || [];
+
+      //Add album to the collection
+      await axios.patch(`http://localhost:5005/users/${currentUser}`, {
+        collection: [album, ...userCollection],
+      });
+      setIsInCollection(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //JSON Server Remove album from collection
+  async function removeAlbumFromCollection() {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:5005/users/${currentUser}`
+      );
+      const userCollection = data.collection || [];
+
+      // remove album from the collection
+      const updatedCollection = userCollection.filter(
+        (item) => item.id !== albumId
+      );
+      await axios.patch(`http://localhost:5005/users/${currentUser}`, {
+        collection: updatedCollection,
+      });
+
+      setIsInCollection(false);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   //handle submit comment
   async function handleNewComment(e) {
     e.preventDefault();
-    const newCommentData = {
-      id: uuidv4(),
-      comment: newComment,
-      userId: currentUser,
-      created: dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT"),
-      albumId: albumId,
-    };
-    try {
-      const { data } = await axios.post(
-        "http://localhost:5005/comments",
-        newCommentData
-      );
-      setComments([data, ...comments]);
-      setNewComment("");
-    } catch (error) {
-      console.log(error);
+    if (newComment !== "") {
+      const newCommentData = {
+        id: uuidv4(),
+        comment: newComment,
+        userId: currentUser,
+        created: dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT"),
+        albumId: albumId,
+      };
+      try {
+        const { data } = await axios.post(
+          "http://localhost:5005/comments",
+          newCommentData
+        );
+        setComments([data, ...comments]);
+        setNewComment("");
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
   //////////------DETAIL PAGE--------/////
@@ -145,112 +207,132 @@ const AlbumDetail = ({ access }) => {
     return <div>Loading album information...</div>;
   }
   return (
-    <div>
+    <div className="album-detail">
       {/* Album presentation */}
       <div className="header-container">
         <img src={album.images[1].url} alt={album.name} />
         <div className="header-title">
           <h1>{album.name}</h1>
           <h2>{album.artists[0].name} </h2>
-          <h3>Label: {album.label}</h3>
-          <Button>
-            <Headphones />
-            See on Spotify
-          </Button>
-          <Button>
-            <CirclePlus />
-            Add to my collection
-          </Button>
+          <h5>Label: {album.label}</h5>
+          <div className="btn-container">
+            <Link
+              to={album.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button className="btn">
+                <Headphones />
+                See on Spotify
+              </Button>
+            </Link>
+            <Button
+              className="btn"
+              variant={isInCollection ? "secondary" : ""}
+              onClick={
+                isInCollection
+                  ? removeAlbumFromCollection
+                  : addAlbumToCollection
+              }
+            >
+              {isInCollection ? <CircleMinus /> : <CirclePlus />}
+              {isInCollection
+                ? "Remove from my collection"
+                : "Add to my collection"}
+            </Button>
+          </div>
         </div>
       </div>
-      <div>
-        {/* Track list */}
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>See the track list</AccordionTrigger>
-            <AccordionContent>
-              <div className="Tracks container"></div>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-[100px]">#</TableHead>
-                    <TableHead>Track</TableHead>
-                    <TableHead>Artists</TableHead>
-                    <TableHead className="text-right">Duration</TableHead>
+      <div className="tabs">
+        <Tabs defaultValue="tracks" className="w-full">
+          <TabsList>
+            <TabsTrigger value="tracks">Tracklist</TabsTrigger>
+            <TabsTrigger value="comments">Comments</TabsTrigger>
+          </TabsList>
+          <TabsContent value="tracks">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">#</TableHead>
+                  <TableHead>Track</TableHead>
+                  <TableHead>Artists</TableHead>
+                  <TableHead className="text-right">Duration</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {album.tracks.items.map((track) => (
+                  <TableRow key={track.track_number}>
+                    <TableCell className="font-medium">
+                      {track.track_number}
+                    </TableCell>
+                    <TableCell>{track.name}</TableCell>
+                    <TableCell>
+                      {track.artists.map((artist) => artist.name).join(", ")}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {convertDuration(track.duration_ms)}
+                    </TableCell>
                   </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {album.tracks.items.map((track) => (
-                    <TableRow key={track.track_number}>
-                      <TableCell className="font-medium">
-                        {track.track_number}
-                      </TableCell>
-                      <TableCell>{track.name}</TableCell>
-                      <TableCell>
-                        {track.artists.map((artist) => artist.name).join(", ")}
-                      </TableCell>
-                      <TableCell className="text-right">
-                        {convertDuration(track.duration_ms)}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>See comments</AccordionTrigger>
-
-            <AccordionContent></AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        Add a comment
-        <div className="comment-box">
-          <Textarea
-            placeholder="Type your comment here."
-            value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
-          />
-          <Button onClick={handleNewComment}>
-            <Send />
-            Send
-          </Button>
-
-          <div className="all-comments">
-            {comments &&
-              users &&
-              comments.map((comment) => {
-                // console.log("before getting user id", comment.userId);
-                // const user = getUsersbyId(comment.userId);
-                // console.log("user found in comments", user);
-                return (
-                  <Card key={comment.id}>
-                    <CardHeader>
-                      <CardTitle className="flex">
-                        <Avatar>
-                          <AvatarImage src="https://github.com/shadcn.png" />
-                          <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        USERname {comment.userId}
-                        {/* <Avatar>
+                ))}
+              </TableBody>
+            </Table>
+          </TabsContent>
+          <TabsContent value="comments">
+            <h4>Add a comment...</h4>
+            <div className="comment-box">
+              <div className="text-area">
+                <Textarea
+                  placeholder="Type your comment here."
+                  value={newComment}
+                  onChange={(e) => setNewComment(e.target.value)}
+                />
+              </div>
+              <Button className="btn" onClick={handleNewComment}>
+                <Send />
+                Send
+              </Button>
+            </div>
+            <div className="all-comments">
+              {comments &&
+                users &&
+                comments.map((comment) => {
+                  // console.log("before getting user id", comment.userId);
+                  // const user = getUsersbyId(comment.userId);
+                  // console.log("user found in comments", user);
+                  return (
+                    <Card className="comment-card" key={comment.id}>
+                      <CardHeader>
+                        <CardTitle className="flex items-center">
+                          <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                          </Avatar>
+                          <div className="user-name">
+                            USERname {comment.userId}
+                            <span className="creation-date">
+                              @ {comment.created}
+                            </span>
+                          </div>
+                          {/* <Avatar>
                           <AvatarImage src={user.name} />
                           <AvatarFallback>{user.name}</AvatarFallback>
                           </Avatar> */}
-                        {/* Test */}
-                        {/* {user.name || "Anonymous"} */}
-                        {/* {getUsersbyId(comment.id).name || "Anonymous"} */}
-                        {/* {test || "Anonymous"} */}
-                      </CardTitle>
-                      <CardDescription>{comment.created}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                      <p>{comment.comment}</p>
-                    </CardContent>
-                  </Card>
-                );
-              })}
-          </div>
-        </div>
+                          {/* Test */}
+                          {/* {user.name || "Anonymous"} */}
+                          {/* {getUsersbyId(comment.id).name || "Anonymous"} */}
+                          {/* {test || "Anonymous"} */}
+                        </CardTitle>
+                        <CardDescription></CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        <p>{comment.comment}</p>
+                      </CardContent>
+                    </Card>
+                  );
+                })}
+            </div>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
